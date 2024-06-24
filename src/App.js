d5e49b1e23c2ee9485304253c961455ecc1439d4
kasianiemiec <kasia.a.niemiec@gmail.com>
2024-06-24 20:40:41 +0300

    added a paragraph with link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <p>Weather App</p>
+      Weather App
+      <p>
+        This project was coded by Kasia Niemiec and it's open sourced on{" "}
+        <a
+          href="https://github.com/kasianiemiec/react-weather-app"
+          target="_blank"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://meek-ganache-a8b772.netlify.app/" target="_blank">
+          Netlify
+        </a>
+        .
+      </p>
     </div>
   );
 }
-
-export default App;
