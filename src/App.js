54780d0993470e2d6c3a7632efa7ee8887fdc80e
kasianiemiec <kasia.a.niemiec@gmail.com>
2024-06-25 10:40:05 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <p>
-        This project was coded by Kasia Niemiec and it's open sourced on{" "}
-        <a
-          href="https://github.com/kasianiemiec/react-weather-app"
-          target="_blank"
-        >
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a href="https://meek-ganache-a8b772.netlify.app/" target="_blank">
-          Netlify
-        </a>
-        .
-      </p>
+      <div className="container">
+        Weather App
+        <p>
+          This project was coded by Kasia Niemiec and it's open-sourced on{" "}
+          <a
+            href="https://github.com/kasianiemiec/react-weather-app"
+            target="_blank"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://meek-ganache-a8b772.netlify.app/" target="_blank">
+            Netlify
+          </a>
+          .
+        </p>
+      </div>
     </div>
   );
 }
